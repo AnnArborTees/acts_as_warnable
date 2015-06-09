d56c61e4f09c727a18e70cd0c44132496dd627b1
Nigel Baillie <metreckk@gmail.com>
2015-06-09 18:11:35 -0400

    make warnable records able to protect their methods with warnings

--- lib/acts_as_warnable.rb
+++ lib/acts_as_warnable.rb
@@ -12,10 +12,33 @@ module ActsAsWarnable
     def acts_as_warnable(options = {})
       has_many :warnings, as: :warnable
       include WarnableInstanceMethods
+      extend WarnableClassMethods
+    end
+  end
+
+  module WarnableClassMethods
+    def warn_on_failure_of(*methods)
+      methods.each do |method_name|
+        define_method "#{method_name}_with_warning" do |*args, &block|
+          begin
+            send("#{method_name}_without_warning", *args, &block)
+          rescue Exception => e
+            issue_warning(
+              warning_source(method_name),
+              "#{e.class.name}: #{e.message}\n\n#{e.backtrace.join("\n")}"
+            )
+          end
+        end
+        alias_method_chain method_name, :warning
+      end
     end
   end
 
   module WarnableInstanceMethods
+    def warning_source(method_name)
+      "#{self.class.name}##{method_name}"
+    end
+
     def issue_warning(source, message)
       warning = warnings.create(source: source, message: message)
 
--- spec/lib/acts_as_warnable_spec.rb
+++ spec/lib/acts_as_warnable_spec.rb
@@ -8,6 +8,27 @@ describe ActsAsWarnable do
     it { is_expected.to respond_to(:issue_warning) }
   end
 
+  describe '.warn_on_failure_of' do
+    before do
+      TestObject.class_eval do
+        def test_method
+          raise "Oh, no!" if false != true
+        end
+        warn_on_failure_of :test_method
+      end
+    end
+    subject { TestObject.create(name: 'jim') }
+
+    context 'given the name of a method' do
+      it 'wraps that method in a begin/rescue that issues a warning on rescue' do
+        expect(subject.warnings.size).to eq 0
+        expect{subject.test_method}.to_not raise_error
+        expect(subject.warnings.size).to eq 1
+        expect(subject.warnings.first.message).to include "Oh, no!"
+      end
+    end
+  end
+
   describe '#issue_warning' do
     subject { TestObject.create(name: 'bill') }
 

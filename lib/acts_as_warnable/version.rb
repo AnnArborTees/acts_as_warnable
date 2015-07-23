7f107247ef61bd16f1f1ac1b14523d7a974aaac8
Nigel Baillie <metreckk@gmail.com>
2015-07-23 11:51:05 -0400

    add timestamps to warnings

--- lib/acts_as_warnable/version.rb
+++ lib/acts_as_warnable/version.rb
@@ -1,3 +1,3 @@
 module ActsAsWarnable
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
--- lib/generators/acts_as_warnable/warning_generator.rb
+++ lib/generators/acts_as_warnable/warning_generator.rb
@@ -3,5 +3,6 @@ class ActsAsWarnable::WarningGenerator < Rails::Generators::Base
 
   def create_migration
     generate 'migration', 'CreateWarnings warnable_id:integer warnable_type:string source:string message:text dismissed_at:datetime dismisser_id:integer'
+    generate 'migration', 'AddTimestampsToWarnings created_at:datetime updated_at:datetime'
   end
 end

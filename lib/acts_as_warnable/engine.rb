896e2d34a7f592b55a2d45bd1908eb35d2cdf27a
Ricky Winowiecki <ricky@annarbortees.com>
2024-10-01 15:56:52 -0400

    (Hot) Requirement of the file needed for Rails 7

--- lib/acts_as_warnable/engine.rb
+++ lib/acts_as_warnable/engine.rb
@@ -1,8 +1,10 @@
 module ActsAsWarnable
   class Engine < ::Rails::Engine
     isolate_namespace ActsAsWarnable
+
     initializer 'acts_as_warnable.action_controller' do |app|
       ActiveSupport.on_load :action_controller do
+        require_relative '../../app/helpers/acts_as_warnable/application_helper'
         helper ActsAsWarnable::ApplicationHelper
       end
     end

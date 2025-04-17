daaa9ba7f3dc0938c85e3d036db6e74d5cfda7d2
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2025-04-17 14:58:28 -0400

    Update engine.rb

--- lib/acts_as_warnable/engine.rb
+++ lib/acts_as_warnable/engine.rb
@@ -2,6 +2,12 @@ module ActsAsWarnable
   class Engine < ::Rails::Engine
     isolate_namespace ActsAsWarnable
 
+    initializer 'acts_as_warnable.register_md_mime' do
+      unless Mime::Type.lookup_by_extension(:md)
+        Mime::Type.register "text/markdown", :md
+      end
+    end
+    
     initializer 'acts_as_warnable.action_controller' do |app|
       ActiveSupport.on_load :action_controller do
         require_relative '../../app/helpers/acts_as_warnable/application_helper'

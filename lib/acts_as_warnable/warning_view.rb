e3deefccdc1e16ec6e7e50983ad57f4abfba39de
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2025-05-29 11:56:22 -0400

    Update warning_view.rb

--- lib/acts_as_warnable/warning_view.rb
+++ lib/acts_as_warnable/warning_view.rb
@@ -1,25 +1,29 @@
 module ActsAsWarnable
   class WarningView
-    # This class defines helpers that can be called within any warning markdown template.
-
     def initialize(warning, object, path)
       @warning = warning
       @warnable = object
       @view_path = path
     end
 
-   def render(template:, locals: {}, formats: [:md, :html])
+    def render(template:, locals: {}, formats: [:md])
       lookup_context = ActionView::LookupContext.new(@view_path)
       lookup_context.formats = formats
-    
-      view_class = Class.new(ActionView::Base.with_empty_template_cache.with_view_paths(@view_path))
-    
-      # Add dynamic methods to the instance
-      view = view_class.new
+
+      # Create a fresh view instance
+      view = ActionView::Base.with_view_paths(@view_path)
+
+      # Expose `warning` and the warnable object in the template context
       view.define_singleton_method(:warning) { @warning }
-      view.define_singleton_method(@warnable.class.name.underscore) { @warnable } if @warnable
-    
-      view.render(template: template, formats: formats, locals: locals)
+      if @warnable
+        view.define_singleton_method(@warnable.class.name.underscore) { @warnable }
+      end
+
+      view.render(
+        template: template,
+        formats: formats,
+        locals: locals
+      )
     end
   end
 end
